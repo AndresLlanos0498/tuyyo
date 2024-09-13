34360cac03efc55e159294989845a090c27fdcbf
AndresLlanos0498 <andres.enrique98@gmail.com>
2024-09-12 22:23:46 -0500

    a

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,6 @@ function Routes(){
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/13072024" exact component={DetailsLove} />
-                <Route path="/details/:type/:id" exact component={Details} />
             </Switch>        
         </BrowserRouter>
     );
