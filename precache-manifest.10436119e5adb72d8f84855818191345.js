eeb6433f29f60c5c89396e89892d471ec5bc3aeb
AndresLlanos0498 <andres.enrique98@gmail.com>
2024-09-12 21:36:07 -0500

    Updates

---
+++ precache-manifest.10436119e5adb72d8f84855818191345.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b21cbf4f0d68cf1892af82d19eab41d",
+    "url": "/tuyyo/index.html"
+  },
+  {
+    "revision": "b54c4c06fa199c16525a",
+    "url": "/tuyyo/static/css/main.d6761f4e.chunk.css"
+  },
+  {
+    "revision": "48d9ff0f760b7063d9d4",
+    "url": "/tuyyo/static/js/2.d48abf58.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tuyyo/static/js/2.d48abf58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54c4c06fa199c16525a",
+    "url": "/tuyyo/static/js/main.01305af9.chunk.js"
+  },
+  {
+    "revision": "ee64db2d0408a6fae822",
+    "url": "/tuyyo/static/js/runtime-main.d31eef37.js"
+  },
+  {
+    "revision": "f42807c11b0bd8a82d141e99232e8a1d",
+    "url": "/tuyyo/static/media/icon-amazon.f42807c1.png"
+  },
+  {
+    "revision": "262e5be1ba67422687dc70b101d0291a",
+    "url": "/tuyyo/static/media/icon-netflix.262e5be1.png"
+  }
+]);
